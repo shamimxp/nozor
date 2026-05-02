bc58f89078b1ec5fc983a96bb4c86d5c42fea6ec
shamimxp <shamim.dev2@gmail.com>
2026-05-02 23:55:34 +0600

    frontend  setup

--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -111,19 +111,19 @@
                                     </select>
                                 </form>
                             </div>
+{{--                            <div class="header-action-icon-2">--}}
+{{--                                <a href="shop-compare.html">--}}
+{{--                                    <img class="svgInject" alt="Nest" src="{{asset('web')}}/assets/imgs/theme/icons/icon-compare.svg" />--}}
+{{--                                    <span class="pro-count blue">3</span>--}}
+{{--                                </a>--}}
+{{--                                <a href="shop-compare.html"><span class="lable ml-0">Compare</span></a>--}}
+{{--                            </div>--}}
                             <div class="header-action-icon-2">
-                                <a href="shop-compare.html">
-                                    <img class="svgInject" alt="Nest" src="{{asset('web')}}/assets/imgs/theme/icons/icon-compare.svg" />
-                                    <span class="pro-count blue">3</span>
-                                </a>
-                                <a href="shop-compare.html"><span class="lable ml-0">Compare</span></a>
-                            </div>
-                            <div class="header-action-icon-2">
-                                <a href="shop-wishlist.html">
+                                <a href="{{route('wishlist')}}">
                                     <img class="svgInject" alt="Nest" src="{{asset('web')}}/assets/imgs/theme/icons/icon-heart.svg" />
                                     <span class="pro-count blue">6</span>
                                 </a>
-                                <a href="shop-wishlist.html"><span class="lable">Wishlist</span></a>
+                                <a href="{{route('wishlist')}}"><span class="lable">Wishlist</span></a>
                             </div>
                             <div class="header-action-icon-2">
                                 <a class="mini-cart-icon" href="shop-cart.html">
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::get('/', function () {
 //    return view('welcome');
     return view('frontend.index');
 });
+
+Route::get('wishlist',[\App\Http\Controllers\FrontendController::class,'wishlist'])->name('wishlist');
+
+
+
 Route::get('/pos/sale', [\App\Http\Controllers\Admin\AdminController::class, 'pos'])->middleware('auth:admin')->name('admin.pos');
 Route::get('/pos/get-products', [\App\Http\Controllers\Admin\AdminController::class, 'getPosProducts'])->middleware('auth:admin')->name('admin.pos.getProducts');
 Route::get('/pos/get-subcategories/{id}', [\App\Http\Controllers\Admin\AdminController::class, 'getPosSubcategories'])->middleware('auth:admin')->name('admin.pos.getSubcategories');
