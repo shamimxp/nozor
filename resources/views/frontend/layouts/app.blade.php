c79a32e7d8b138eb0b879cddd2bdcf427ce40944
shamimxp <shamim.dev2@gmail.com>
2026-05-05 01:01:47 +0600

    feat: make quick view modal dynamic

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -32,14 +32,14 @@
                     <div class="deal-content detail-info">
                         <h4 class="product-title"><a href="shop-product-right.html" class="text-heading">Organic fruit for your family's health</a></h4>
                         <div class="clearfix product-price-cover">
-                            <div class="product-price primary-color float-left">
-                                <span class="current-price text-brand">$38</span>
-                                <span>
-                                        <span class="save-price font-md color3 ml-15">26% Off</span>
-                                        <span class="old-price font-md ml-15">$52</span>
-                                    </span>
+                                <div class="product-price primary-color float-left">
+                                    <span class="current-price text-brand" id="qv-final-price"></span>
+                                    <span id="qv-old-price-container" style="display:none;">
+                                            <span class="save-price font-md color3 ml-15" id="qv-discount-label"></span>
+                                            <span class="old-price font-md ml-15" id="qv-old-price"></span>
+                                        </span>
+                                </div>
                             </div>
-                        </div>
                     </div>
                     <div class="deal-bottom">
                         <p class="mb-20">Hurry Up! Offer End In:</p>
@@ -72,46 +72,18 @@
                         <div class="detail-gallery">
                             <span class="zoom-icon"><i class="fi-rs-search"></i></span>
                             <!-- MAIN SLIDES -->
-                            <div class="product-image-slider">
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-2.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-1.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-3.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-4.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-5.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-6.jpg" alt="product image" />
-                                </figure>
-                                <figure class="border-radius-10">
-                                    <img src="{{asset('web')}}/assets/imgs/shop/product-16-7.jpg" alt="product image" />
-                                </figure>
+                            <div class="product-image-slider" id="qv-product-image-slider">
                             </div>
                             <!-- THUMBNAILS -->
-                            <div class="slider-nav-thumbnails">
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-3.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-4.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-5.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-6.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-7.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-8.jpg" alt="product image" /></div>
-                                <div><img src="{{asset('web')}}/assets/imgs/shop/thumbnail-9.jpg" alt="product image" /></div>
+                            <div class="slider-nav-thumbnails" id="qv-slider-nav-thumbnails">
                             </div>
                         </div>
                         <!-- End Gallery -->
                     </div>
                     <div class="col-md-6 col-sm-12 col-xs-12">
                         <div class="detail-info pr-30 pl-30">
-                            <span class="stock-status out-stock"> Sale Off </span>
-                            <h3 class="title-detail"><a href="shop-product-right.html" class="text-heading">Seeds of Change Organic Quinoa, Brown</a></h3>
+                            <span class="stock-status out-stock" id="qv-discount" style="display:none;"> Sale Off </span>
+                            <h3 class="title-detail"><a href="#" class="text-heading" id="qv-title"></a></h3>
                             <div class="product-detail-rating">
                                 <div class="product-rate-cover text-end">
                                     <div class="product-rate d-inline-block">
@@ -141,8 +113,7 @@
                             </div>
                             <div class="font-xs">
                                 <ul>
-                                    <li class="mb-5">Vendor: <span class="text-brand">Nest</span></li>
-                                    <li class="mb-5">MFG:<span class="text-brand"> Jun 4.2022</span></li>
+                                    <li class="mb-5">Category: <span class="text-brand" id="qv-category"></span></li>
                                 </ul>
                             </div>
                         </div>
@@ -207,6 +178,82 @@
 <!-- Template  JS -->
 <script src="{{asset('web')}}/assets/js/main.js?v=5.5"></script>
 <script src="{{asset('web')}}/assets/js/shop.js?v=5.5"></script>
+<script>
+    $(document).on('click', '.quick-view-btn', function (e) {
+        e.preventDefault();
+        var id = $(this).data('product-id');
+        $.ajax({
+            url: "/product/quick-view/" + id,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                $('#qv-title').text(data.name);
+                $('#qv-title').attr('href', data.detail_url);
+                $('#qv-category').text(data.category);
+                $('#qv-final-price').text(data.final_price);
+
+                if (data.discount) {
+                    $('#qv-discount').text('Sale Off').show();
+                    $('#qv-discount-label').text(data.discount);
+                    $('#qv-old-price').text(data.price);
+                    $('#qv-old-price-container').show();
+                } else {
+                    $('#qv-discount').hide();
+                    $('#qv-old-price-container').hide();
+                }
+
+                if ($('#qv-product-image-slider').hasClass('slick-initialized')) {
+                    $('#qv-product-image-slider').slick('unslick');
+                }
+                if ($('#qv-slider-nav-thumbnails').hasClass('slick-initialized')) {
+                    $('#qv-slider-nav-thumbnails').slick('unslick');
+                }
+
+                var sliderHtml = '';
+                var thumbHtml = '';
+                $.each(data.gallery, function (key, value) {
+                    sliderHtml += '<figure class="border-radius-10"><img src="' + value + '" alt="product image" /></figure>';
+                    thumbHtml += '<div><img src="' + value + '" alt="product image" /></div>';
+                });
+                
+                if(data.gallery.length === 0 && data.featured_image) {
+                     sliderHtml += '<figure class="border-radius-10"><img src="' + data.featured_image + '" alt="product image" /></figure>';
+                     thumbHtml += '<div><img src="' + data.featured_image + '" alt="product image" /></div>';
+                }
+
+                $('#qv-product-image-slider').html(sliderHtml);
+                $('#qv-slider-nav-thumbnails').html(thumbHtml);
+
+                $('#qv-product-image-slider').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    fade: false,
+                    asNavFor: '#qv-slider-nav-thumbnails',
+                });
+
+                $('#qv-slider-nav-thumbnails').slick({
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    asNavFor: '#qv-product-image-slider',
+                    dots: false,
+                    focusOnSelect: true,
+                    prevArrow: '<button type="button" class="slick-prev"><i class="fi-rs-arrow-small-left"></i></button>',
+                    nextArrow: '<button type="button" class="slick-next"><i class="fi-rs-arrow-small-right"></i></button>'
+                });
+                
+                $('#qv-slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+                $('#qv-slider-nav-thumbnails .slick-slide').eq(0).addClass('slick-active');
+                $('#qv-product-image-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+                    var mySlideNumber = nextSlide;
+                    $('#qv-slider-nav-thumbnails .slick-slide').removeClass('slick-active');
+                    $('#qv-slider-nav-thumbnails .slick-slide').eq(mySlideNumber).addClass('slick-active');
+                });
+            }
+        });
+    });
+</script>
+
 </body>
 
 </html>
